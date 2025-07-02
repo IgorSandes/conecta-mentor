2942fa89b00679f78db1fa1e54f0340955a830f4
Igor Sandes <igorsandes213@gmail.com>
2025-07-02 09:31:10 -0300

    refatoração do arquivo app.js e server.js

--- APIs/conecta-mentor-backend/app.js
+++ APIs/conecta-mentor-backend/app.js
@@ -1,28 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 
-// Criação do app
-const app = express();
-
-// Middlewares globais
-app.use(cors());
-app.use(express.json());
-
-// Importa rotas
-import criarUsuarioRouters from './rotas/criarUsuario.js'
+import criarUsuarioRouter from './rotas/criarUsuario.js';
 import autenticacaoRoutes from './rotas/autenticacao.js';
 import usuariosRoutes from './rotas/usuarios.js';
-import definirPerfilRoutesRoutes from './rotas/definirPerfil.js';
+import definirPerfilRoutes from './rotas/definirPerfil.js';
 import verificarPerfilRoutes from './rotas/verificarPerfil.js';
 import testeRoutes from './rotas/teste.js';
 
-// Usa as rotas com prefixo /api
-app.use('/api', criarUsuarioRouters);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', criarUsuarioRouter);
 app.use('/api', autenticacaoRoutes);
 app.use('/api', usuariosRoutes);
-app.use('/api', definirPerfilRoutesRoutes);
+app.use('/api', definirPerfilRoutes);
 app.use('/api', verificarPerfilRoutes);
 app.use('/api', testeRoutes);
 
-// Exporta o app
 export default app;
--- APIs/conecta-mentor-backend/server.js
+++ APIs/conecta-mentor-backend/server.js
@@ -1,27 +1,7 @@
 import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-
-import criarUsuarioRouter from './rotas/criarUsuario.js';
-import autenticacaoRoutes from './rotas/autenticacao.js';
-import usuariosRoutes from './rotas/usuarios.js';
-import definirPerfilRoutes from './rotas/definirPerfil.js';
-import verificarPerfilRoutes from './rotas/verificarPerfil.js';
-import testeRoutes from './rotas/teste.js';
+import app from './app.js';
 
 dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Usa as rotas com prefixo /api
-app.use('/api', criarUsuarioRouter);
-app.use('/api', autenticacaoRoutes);
-app.use('/api', usuariosRoutes);
-app.use('/api', definirPerfilRoutes);
-app.use('/api', verificarPerfilRoutes);
-app.use('/api', testeRoutes);
 
 const PORT = process.env.PORT || 3000;
 
